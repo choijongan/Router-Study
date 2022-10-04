5edd67117874c48200a95490f14f38b3bf813604
choijongan <mmm7134@naver.com>
2022-10-04 14:51:22 +0900

    파라미터 값 읽어오는 법

--- router-example/src/page/ProductDetailPage.js
+++ router-example/src/page/ProductDetailPage.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom' 
+//useParams의 리액터훅을 이용하면 파라미터 아이디값 가져다 보여줄수 있다.
 const ProductDetailPage = () => {
+ const {id} = useParams();
+ console.log("ppp", useParams);
+
   return (
     <div>
-        <h1>Show Product Detail</h1>
+        <h1>Show Product Detail{id}</h1>
     </div>
   )
 }
