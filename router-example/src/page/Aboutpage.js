7701b73091012855650ea079ef7ed33a988d9813
choijongan <mmm7134@naver.com>
2022-10-04 13:44:36 +0900

    페이지사이 이동

--- router-example/src/page/Aboutpage.js
+++ router-example/src/page/Aboutpage.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Aboutpage = () => {
+    const navigate = useNavigate() //함수안에서 써야되면 useNavigate
+
+    const goToHomepage=()=> {
+      navigate("/")
+    };
+
   return (
     <div>
         <h1>Aboutpage</h1>
+        <button onClick={goToHomepage}>Go to Homepage</button>
     </div>
   )
 }
--- router-example/src/page/Homepage.js
+++ router-example/src/page/Homepage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Homepage = () => {
+const Homepage = () => { //버튼누르면 함수호출없이 바로가도되면 Link
   return (
     <div>
         <h1>Homepage</h1>
+        <Link to="/about">Go to about page</Link> 
     </div>
   )
 }
