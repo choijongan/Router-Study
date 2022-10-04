a5fcbaa07369f129d6fdf2629f0d1679901e71b9
choijongan <mmm7134@naver.com>
2022-10-04 16:17:03 +0900

    쿼리값 가져오는 방법

--- router-example/src/page/Homepage.js
+++ router-example/src/page/Homepage.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Homepage = () => { //버튼누르면 함수호출없이 바로가도되면 Link
+    const navigate = useNavigate()
+    const goProductPage=()=>{
+      navigate('/products?a=pants')
+    }
   return (
     <div>
         <h1>Homepage</h1>
         <Link to="/about">Go to about page</Link> 
+        <button onClick={goProductPage}>go to product page</button>
     </div>
   )
 }
